d201ac059dbaccbbf22a57bad89a1559462072e9
Luke Smetham <luke@arkitek.co.uk>
2020-10-05 07:35:55 +0200

    init @combase/ui, added storybook to workspace

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+    stories: ['../packages/**/src/**/*.stories.js', '../packages/**/src/**/*.story.js'],
+    addons: ['@storybook/addon-docs'],
+};
---
+++ packages/ui/src/Button/Button.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Button } from ".";
+
+export default {
+  component: Button,
+  title: "UI/Button",
+};
+
+export const primary = () => <Button>{"Button"}</Button>;
