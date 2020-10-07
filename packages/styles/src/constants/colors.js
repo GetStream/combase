6e7d896562ac7ba5fa9a63e12b129b7b713abe7b
Luke Smetham <luke@smetham.dev>
2020-10-07 19:21:22 +0200

    added more colors to the styles constants

--- packages/styles/src/constants/colors.js
+++ packages/styles/src/constants/colors.js
@@ -1,5 +1,12 @@
 export const colors = {
     black: '#000000',
     blue: '#4D7CFE',
+    gray: "#ECEDF2",
+    green: '#41F37F',
+    lavender: '#848EA9',
+    offWhite: '#FCFCFC',
+    red: '#F34141',
+    slate: '#282B33',
     white: '#ffffff',
+    yellow: '#F3CC41',
 };
